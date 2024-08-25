d864c251555f31fdd09d865b2bcce487d6b09329
kjch <1834423612@qq.com>
2024-08-25 06:53:28 +0000

    revert: del feat: TailwindCSS config add '300small'(max:374px) screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,6 @@ export default {
     "./Data/**/*.{js,ts,jsx,tsx,json}",
   ],
   theme: {
-    screens: {
-      '300small': {'max': '374px'},
-    },
     extend: {
       fontFamily: {
         heading: ['Manrope', ...fontFamily.sans],
