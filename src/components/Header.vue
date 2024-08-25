5965baeda8f3832f940c4ee7fb74b594f36a7f89
kjch <1834423612@qq.com>
2024-08-25 23:17:29 +0000

    fix: Header Title click direct

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,16 @@
 <template>
   <header class="bg-primary text-primary-foreground py-4 px-4 sm:px-8 flex items-center">
     <div class="container mx-auto flex items-center justify-between">
-      <div class="flex items-center gap-1 space-x-2">
-        <router-link to="/" class="hover:no-underline">
+      <div class="gap-1 space-x-2">
+        <router-link to="/" class="hover:no-underline flex flex-row items-center">
           <!-- 
             - `w-10 h-10` sets the width and height of the logo to 40px by default (e.g., tablets and desktops) (Tailwind's `w-10` and `h-10` correspond to 2.5rem, which is 40px).
             - `sm:w-8 sm:h-8` sets the width and height of the logo to 32px on small screens (phones) (`w-8` and `h-8` correspond to 2rem, which is 32px)
           -->
           <img src="@/assets/images/bison-robotics.png" alt="695 Logo"
             class="w-7 h-7 md:w-10 md:h-10 sm:w-8 sm:h-8 rounded-full" />
+          <h1 class="ml-2 md:ml-3 leading-4 text-size-5 sm:text-xl md:text-2xl font-bold">695 Bison Robotics</h1>
         </router-link>
-        <h1 class="leading-4 text-size-5 sm:text-xl md:text-2xl font-bold">695 Bison Robotics</h1>
       </div>
 
       <!-- Mobile Dropdown Menu v-if="!isDesktop" -->
