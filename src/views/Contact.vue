d9fa9f3e9929eba38028feed985f2db4cf8dfb81
kjch <1834423612@qq.com>
2024-08-29 23:36:35 +0000

    fix: Contact Page Card background & add location map

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -25,17 +25,33 @@
                         <FeedBack />
                     </div>
                     <div class="space-y-6">
-                        <div class="rounded-lg border-4 border-blue-400 bg-yellow-50 p-6 shadow-xl">
+                        <div class="rounded-lg border-4 border-blue-400 bg-blue-100 p-6 shadow-xl">
                             <h2 class="mb-4 text-2xl font-semibold text-gray-900">Chat with us</h2>
-                            <div class="mb-4 flex items-center space-x-4">
-                                <!--<img alt="Team Coach" class="rounded-full" height="64"
+
+                            <div class="rounded-2xl bg-gray-50 p-10">
+                                <h3 class="text-base font-semibold leading-7 text-gray-900">Collaborate</h3>
+                                <dl class="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+                                    <div>
+                                        <dt class="sr-only">Email</dt>
+                                        <dd><a class="font-semibold text-indigo-600"
+                                                href="mailto:collaborate@example.com">collaborate@example.com</a></dd>
+                                    </div>
+                                    <div class="mt-1">
+                                        <dt class="sr-only">Phone number</dt>
+                                        <dd>+1 (555) 905-2345</dd>
+                                    </div>
+                                </dl>
+                            </div>
+
+                            <!-- <div class="mb-4 flex items-center space-x-4">
+                                <img alt="Team Coach" class="rounded-full" height="64"
                                     src="https://placehold.co/64x64/3b82f6/white"
-                                    style="aspect-ratio: 64/64; object-fit: cover;" width="64" />-->
+                                    style="aspect-ratio: 64/64; object-fit: cover;" width="64" />
                                 <div>
                                     <h3 class="font-semibold text-gray-800">Coach John</h3>
                                     <p class="text-sm text-gray-600">Head Coach</p>
 
-                                    <!-- Icons -->
+                                    Icons
                                     <div class="flex space-x-4 mt-1 text-black hover:text-gray-400">
                                         <a href="mailto:coach@frc695.com" class="text-black hover:text-gray-600">
                                             <span class="sr-only">Email</span>
@@ -43,7 +59,7 @@
                                         </a>
                                     </div>
                                 </div>
-                            </div>
+                            </div> -->
 
                             <!-- Chat Button(Only uncomment this when using) -->
                             <!-- <button @click="startChat"
@@ -51,7 +67,7 @@
                                 Chat
                             </button> -->
                         </div>
-                        <div class="rounded-lg border-4 border-emerald-400 bg-yellow-50 p-6 shadow-xl">
+                        <div class="rounded-lg border-4 border-emerald-400 bg-emerald-100 p-6 shadow-xl">
                             <h2 class="mb-4 text-2xl font-semibold text-gray-900">Visit us</h2>
                             <div class="mb-2 flex items-center space-x-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-600"
@@ -63,8 +79,10 @@
                                 <p class="text-gray-800">24601 Fairmount Blvd, Cleveland, OH 44122, USA.</p>
                             </div>
                             <div class="aspect-video overflow-hidden rounded-md bg-yellow-100">
-                                <img alt="Map" src="https://th.bing.com/th/id/OIP.Nj6b7ASE7ZgUSg3lcgwqgQHaFj?rs=1&pid=ImgDetMain"
-                                    class="h-full w-full object-cover" />
+                                <!-- <img alt="Map" src="https://placehold.co/300x400/3b82f6/white"
+                                    class="h-full w-full object-cover" /> -->
+                                
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2988.936050733924!2d-81.50350832399468!3d41.48398608999798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88310289d7a522f5%3A0x6b21ae2be51d4ca1!2sBeachwood%20High%20School!5e0!3m2!1szh-CN!2sus!4v1724974249750!5m2!1szh-CN!2sus" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                             </div>
                         </div>
                     </div>
