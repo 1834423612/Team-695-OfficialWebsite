44968f4afd16dd524fa2066a9f5b43ff15731372
kjch <1834423612@qq.com>
2024-08-30 12:49:32 +0000

    fix: Mentor Card Bg color

--- src/views/Mentors.vue
+++ src/views/Mentors.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <main class="flex-1">
-      <section id="mentors" class="py-16 px-6 bg-green-100">
+      <section id="mentors" class="py-16 px-6 bg-purple-100">
         <div class="container mx-auto max-w-6xl">
           <h2 class="text-3xl font-bold mb-8 text-center">Our Mentors</h2>
           <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
