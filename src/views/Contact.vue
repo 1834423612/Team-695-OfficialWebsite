2408f5436c9cba3aa73c5a167e57730779b0b78f
kjch <1834423612@qq.com>
2024-08-30 01:07:47 +0000

    fix: Contact Page Google Map display Language

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -87,7 +87,7 @@
                                     class="h-full w-full object-cover" /> -->
 
                                 <iframe
-                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2988.936050733924!2d-81.50350832399468!3d41.48398608999798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88310289d7a522f5%3A0x6b21ae2be51d4ca1!2sBeachwood%20High%20School!5e0!3m2!1szh-CN!2sus!4v1724974249750!5m2!1szh-CN!2sus"
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2988.936050733924!2d-81.50350832399468!3d41.48398608999798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88310289d7a522f5%3A0x6b21ae2be51d4ca1!2sBeachwood%20High%20School!5e0!3m2!1sen-US!2sus!4v1724974249750!5m2!1sen-US!2sus"
                                     width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"
                                     referrerpolicy="no-referrer-when-downgrade"></iframe>
                             </div>
