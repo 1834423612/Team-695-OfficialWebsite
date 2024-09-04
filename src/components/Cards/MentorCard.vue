1a39e7e45cb34b4205a43fbf73f948c3359a20a2
kjch <1834423612@qq.com>
2024-09-04 04:56:21 +0000

    feat: Mentor Details Page Breadcrumb

--- src/components/Cards/MentorCard.vue
+++ src/components/Cards/MentorCard.vue
@@ -6,17 +6,16 @@
         {{ name }}
       </h5>
       <span class="text-sm text-gray-500 dark:text-gray-400">{{ role }}</span>
-      <span class="text-sm text-gray-500 dark:text-gray-400">{{ email }}</span>
-      <div class="flex mt-4 md:mt-6">
+      <!-- <span class="text-sm text-gray-500 dark:text-gray-400">{{ email }}</span> -->
+      <div class="flex mt-2 md:mt-2">
         <!-- <a
           :href="detailsLink"
           class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >Details</a
         > -->
-        <a 
-          :href="detailsLink"
-          class="text-blue-500 hover:text-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 inline-flex items-center"
-        >Learn More
+        <a :href="detailsLink"
+          class="text-blue-500 hover:text-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 inline-flex items-center">Learn
+          More
           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
             class="w-4 h-4 ml-2" viewBox="0 0 24 24">
             <path d="M5 12h14M12 5l7 7-7 7"></path>
@@ -59,4 +58,3 @@ export default defineComponent({
   },
 });
 </script>
-
--- src/views/MentorDetail.vue
+++ src/views/MentorDetail.vue
@@ -4,6 +4,33 @@
             <section class="text-gray-600 body-font">
                 <div class="container px-5 pb-24 pt-16 mx-auto flex flex-col">
                     <div class="lg:w-5/6 h-full mx-auto">
+                        
+                        <nav class="flex" aria-label="Breadcrumb">
+                            <ol role="list"
+                                class="flex flex-wrap py-2 space-x-2 rounded-md bg-white px-4 lg:px-6 shadow">
+                                <li>
+                                    <div>
+                                        <a href="/" class="text-gray-400 hover:text-gray-500 flex items-center">
+                                            <Icon icon="ic:round-home" class="h-5 w-5 flex-shrink-0"
+                                                aria-hidden="true" />
+                                            <span class="sr-only">Home</span>
+                                        </a>
+                                    </div>
+                                </li>
+                                <li v-for="page in pages" :key="page.name">
+                                    <div class="flex items-center">
+                                        <Icon icon="iconamoon:arrow-right-2-duotone"
+                                            class="h-5 w-5 flex-shrink-0 text-gray-500" aria-hidden="true" />
+                                        <a :href="page.href" class="ml-2 text-sm font-medium hover:text-gray-700"
+                                            :class="page.current ? 'text-indigo-600 hover:text-indigo-400 hover:underline hover:decoration-2 hover:underline-offset-4' : 'text-gray-500'"
+                                            :aria-current="page.current ? 'page' : undefined">
+                                            {{ page.name }}
+                                        </a>
+                                    </div>
+                                </li>
+                            </ol>
+                        </nav>
+
                         <!--<div class="rounded-lg h-96 overflow-hidden">
                             <img
                                 :alt="mentor.name"
@@ -25,14 +52,19 @@
                                     <h2 class="font-medium title-font mt-4 text-gray-900 text-lg">{{ mentor.name }}</h2>
                                     <div class="w-12 h-1 bg-blue-500 rounded mt-2 mb-4"></div>
                                     <p class="text-base">{{ mentor.role }}</p>
-                                    <a :href="'mailto:' + mentor.email" class="mt-3">
+                                    <a :href="'mailto:' + mentor.email" class="flex mt-3 hover:text-gray-600">
                                         <i-line-md:email-twotone class="text-xl" />
+                                        <span
+                                            class="ml-2 font-base text-indigo-600 hover:text-indigo-500 hover:underline hover:decoration-2 hover:underline-offset-4">
+                                            {{ mentor.email }}
+                                        </span>
                                     </a>
                                 </div>
                             </div>
-                            <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+                            <div
+                                class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
                                 <p class="leading-relaxed text-lg mb-4">{{ mentor.description }}</p>
-                                <p class="text-base">Email: {{ mentor.email }}</p>
+                                <!-- <p class="text-base">Email: {{ mentor.email }}</p> -->
                                 <!-- <a class="text-indigo-500 inline-flex items-center">Learn More
                                     <svg fill="none" stroke="currentColor" stroke-linecap="round"
                                         stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2"
@@ -51,13 +83,26 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { Icon } from '@iconify/vue';
 
 export default defineComponent({
+    components: {
+        Icon
+    },
     props: {
         mentor: {
             type: Object,
             required: true
         }
-    }
+    },
+    data() {
+        return {
+            pages: [
+                // { name: 'Home', href: '/', current: false },
+                { name: 'Members', href: '/members', current: false },
+                { name: this.mentor.name, href: '#', current: true },
+            ]
+        };
+    },
 });
 </script>
