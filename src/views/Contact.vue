989b169d7a637552317a4e189165b79740985926
kjch <1834423612@qq.com>
2024-09-04 03:51:57 +0000

    fix: Contact Page Card layout

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -35,13 +35,14 @@
                                 <dl class="mt-3 space-y-1 text-sm leading-6 text-gray-600">
                                     <div>
                                         <dt class="sr-only">Email</dt>
-                                        <dd>
-                                            <a href="mailto:coach@frc695.com" class="flex text-black hover:text-gray-600">
-                                                <span class="sr-only">Email</span>
-                                                <Icon icon="line-md:email" height="23" width="23" />
-                                                <a class="ml-2 font-semibold text-indigo-600 hover:text-indigo-500 hover:underline hover:decoration-2 hover:underline-offset-4">coach@frc695.com</a>
-                                            </a>
-                                        </dd>
+                                        <a href="mailto:coach@frc695.com" class="flex text-black hover:text-gray-600">
+                                            <span class="sr-only">Email</span>
+                                            <Icon icon="line-md:email" height="23" width="23" />
+                                            <span
+                                                class="ml-2 font-semibold text-indigo-600 hover:text-indigo-500 hover:underline hover:decoration-2 hover:underline-offset-4">
+                                                coach@frc695.com
+                                            </span>
+                                        </a>
                                     </div>
                                     <!-- <div class="mt-1">
                                         <dt class="sr-only">Phone number</dt>
@@ -56,14 +57,18 @@
                                 <span class="text-sm font-semibold text-gray-500">Github Open Source</span>
                                 <dl class="mt-3 space-y-1 text-sm leading-6 text-gray-600">
                                     <div>
-                                        <dt class="sr-only">Email</dt>
-                                        <dd>
-                                            <a href="mailto:coach@frc695.com" class="flex text-black hover:text-gray-600">
-                                                <span class="sr-only">Email</span>
-                                                <Icon icon="line-md:github" height="23" width="23" />
-                                                <a class="ml-2 font-semibold text-indigo-600 hover:text-indigo-500 hover:underline hover:decoration-2 hover:underline-offset-4">coach@frc695.com</a>
-                                            </a>
-                                        </dd>
+                                        <dt class="sr-only">Resource</dt>
+
+                                        <router-link to="/resources"
+                                            class="flex text-black hover:text-gray-600">
+                                            <span class="sr-only">Resource</span>
+                                            <Icon icon="bi:github" height="23" width="23" />
+                                            <span
+                                                class="ml-2 font-semibold text-indigo-600 hover:text-indigo-500 hover:underline hover:decoration-2 hover:underline-offset-4">
+                                                Go to the Resources Page to find more 
+                                            </span>
+                                            <Icon icon="mingcute:arrow-right-line" class="ml-1 mt-1 w-4 h-4" />
+                                        </router-link>
                                     </div>
                                     <!-- <div class="mt-1">
                                         <dt class="sr-only">Phone number</dt>
@@ -99,13 +104,8 @@
                         <div class="rounded-lg border-4 border-emerald-400 bg-emerald-100 p-6 shadow-xl">
                             <h2 class="mb-4 text-2xl font-semibold text-gray-900">Visit us</h2>
                             <div class="mb-2 flex items-center space-x-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-600"
-                                    viewBox="0 0 20 20" fill="currentColor">
-                                    <path fill-rule="evenodd"
-                                        d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                                <p class="text-gray-800">24601 Fairmount Blvd, Beachwood, Cleveland, OH 44122, USA.</p>
+                                <Icon icon="ion:location" class="w-5 h-5 text-gray-600" />
+                                <p class="text-gray-800">25100 Fairmount Blvd, Beachwood, OH 44122, USA.</p>
                             </div>
                             <div class="aspect-video overflow-hidden rounded-md bg-yellow-100">
                                 <!-- <img alt="Map" src="https://placehold.co/300x400/3b82f6/white"
@@ -114,7 +114,8 @@
                                 <iframe
                                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2988.936050733924!2d-81.50350832399468!3d41.48398608999798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88310289d7a522f5%3A0x6b21ae2be51d4ca1!2sBeachwood%20High%20School!5e0!3m2!1sen-US!2sus!4v1724974249750!5m2!1sen-US!2sus"
                                     width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"
-                                    referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                    referrerpolicy="no-referrer-when-downgrade">
+                                </iframe>
                             </div>
                         </div>
                     </div>
